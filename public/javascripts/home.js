6967dd533ebd4615ebbdfb820a3fabc4aec90abd
david <dudejrss@naver.com>
2019-11-17 19:23:35 +0900

    first commit

---
+++ public/javascripts/home.js
@@ -0,0 +1,7 @@
+const movie = ["Jocker", "Parasite", "Shrek", "HarryPotter", "Walkingdead"];
+const move_selected = movie[0];
+
+const Movie_name = document.querySelectorAll('.movie_name')[0];
+Movie_name.innerHTML = move_selected;
+
+
---
+++ public/javascripts/index.js
@@ -0,0 +1,34 @@
+
+
+const Menu_icon = document.querySelector("#menu_icon");
+
+
+
+const toggle_menu = function(event) {
+	const Menu = document.querySelector("#menu");
+	const Nav = document.querySelector('#Nav');
+	const Login = document.querySelector('#login');
+	const Content = document.querySelector('#Content');
+
+	if (Menu.classList.contains("hidden")){
+		setTimeout(()=>{
+			Menu.classList.remove("hidden");
+			Login.classList.add("menuOn");
+			Content.classList.add("menuOn");
+			Nav.classList.add("menuOn");
+		},300);
+	} else{
+		setTimeout(()=>{
+			Menu.classList.add("hidden");
+			Nav.classList.remove("menuOn");
+			Content.classList.remove("menuOn");
+			Login.classList.remove("menuOn");
+		})
+	}
+
+};
+
+
+Menu_icon.addEventListener('click',toggle_menu);
+
+
