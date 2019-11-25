08d04d930b95a55f1afc171715dbd2119f5fa9b8
Jaesun Park <zzangjames75@gmail.com>
2019-11-26 02:39:12 +0900

    로그인과 회원가입, 건의 사항 게시판

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,142 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var session = require('express-session');
+
+var connection = mysql.createConnection({
+    multipleStatements: true,
+    host: 'localhost',
+    user: 'root',
+    post: 3000,
+    password: '',
+    database: 'cenema',
+    multipleStatements: true
+});
+
+//session
+router.use(session({
+    secret: 'sid',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000 * 60 * 60
+    }
+}));
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    if (req.session.user) {
+		console.log(req.session.user);
+        res.render('index', {
+            logined: req.session.user.logined,
+			username: req.session.user.username
+        });
+    }
+    else {
+        res.render('index.ejs', {
+            logined: false,
+            username: " "
+        });
+    }
 });
+
 
 router.get('/login',function(req,res,next){
 	res.render('login');
-})
+});
+
+router.get('/register',function(req,res,next){
+	res.render('register');
+});
 
 router.get('/reserv',function(req,res,next){
 	res.render('reservation');
-})
+});
+
+router.get('/suggestion',function(req,res,next){
+	res.render('suggestion');
+});
 
 module.exports = router;
+
+router.post('/', function(req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+
+    var sql = 'SELECT * FROM member WHERE username = ?';
+    connection.query(sql, [username], function (error, results, fields) {
+        if (results.length == 0) {
+            res.render('login', { alert: true });
+        } else {
+            var db_pwd = results[0].password;
+
+            if (password == db_pwd) {
+                //session
+                req.session.user = {
+                    logined: true,
+                    username: results[0].username
+                }
+
+                res.render('index', {
+                    logined: req.session.user.logined,
+                    username: req.session.user.username
+                });
+            }
+            else {
+                res.render('login', { alert: true });
+            }
+        }
+    });
+})
+
+router.post('/register', function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+	var pwdconf = req.body.pwdconf;
+	var birth = req.body.birth;
+	var sex = req.body.sex;
+	var address = req.body.address;
+
+    if (password !== pwdconf) {
+        res.redirect('register');
+    }
+    else {
+        var sql = 'SELECT * FROM member WHERE username = ?';
+        connection.query(sql, [username], function (error, results, fields) {
+            if (results.length == 0) {
+                connection.query("INSERT INTO member(username, password, birth, sex, address) VALUES(?,?,?,?,?)", [username, password, birth, sex, address], function () {
+                    res.redirect('login');
+                });
+            }
+            else {
+                res.render("register");
+            }
+        });
+    }
+});
+
+router.post('/suggestion_insert', function (req, res) {
+    var title = req.body.title;
+    var content = req.body.content;
+    var writer_name = req.session.user.username;
+
+    var sql = 'INSERT INTO suggestion(title, content, writer_name) VALUES (?,?,?)';
+    connection.query(sql, [title, content, writer_name], function (error, results, fields) {
+        res.redirect('/suggestion');
+    });
+});
+
+router.post('/suggestion/:suggestion_id', function(req, res){
+    if(req.session.user){
+        var notice_id = req.url.split("/")[2];
+        var comment = req.body.comment;
+        var writer_name = req.session.user.user_name;
+        var sql = `INSERT INTO comment(notice_id, comment, writer_name) VALUES (?,?,?) ;`
+        connection.query(sql, [notice_id, comment, writer_name], function(error, results, fields){
+            res.redirect(`/notice/${suggestion_id}`);
+        });
+    }
+    else {
+        res.render('login.ejs');
+    }
+});
