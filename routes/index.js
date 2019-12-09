6c3153ddb22b0fe7a615a60e232f035feaaea352
david <dudejrss@naver.com>
2019-12-09 17:33:41 +0900

    add cacel reservation

--- routes/index.js
+++ routes/index.js
@@ -732,7 +732,7 @@ router.get('/reserv_seat/:box_office_id', function (req, res) {
 
 
 router.get('/api/reserv',function(req,res,next){
-    const sql = "SELECT reservation_id, movie_img, movie_name, cinema_name, screen_no, box_office.date,start_time,running_time, amount from reservation natural join box_office natural join movie natural join cinema natural join screen natural join payment WHERE member_id = (SELECT member_id FROM member WHERE username = ?) ;";
+    const sql = "SELECT reservation_id, movie_img, movie_name, cinema_name, screen_no, box_office.date,start_time,running_time, amount from reservation natural join box_office natural join movie natural join cinema natural join screen natural join payment WHERE member_id = (SELECT member_id FROM member WHERE username = ?) and reservatioN_status = 'R' ;";
     connection.query(sql,[login.username],function(err,results,fields){
         res.json({
             type : "reservation_list",
@@ -740,6 +740,26 @@ router.get('/api/reserv',function(req,res,next){
         });
     });
 })
+
+router.delete('/api/reserv', function(req,res,next){
+    const reservation_id = req.body.reservation_id;
+    if(reservation_id){  
+        const sql = `UPDATE \`cenema\`.\`reservation\` SET \`reservation_status\` = 'C' WHERE (\`reservation_id\` = \'${reservation_id}\');`
+        connection.query(sql,function(err,results,fields){
+            if(err){
+                res.json({
+                    type : "reservation_cancel"
+                });
+            }
+            else {
+                res.json({
+                    type : "reservation_cancel"
+                });
+            }
+        });
+    }
+
+});
 
 
 router.get('/payment',function(req,res,next){
